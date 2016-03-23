c4d751003155468083255e899ca98a26328d6483
dominicsp <domovia1@gmail.com>
2016-03-23 13:32:50 +0530

    Create admins.pp

---
+++ site/users/manifests/admins.pp
@@ -0,0 +1,12 @@
+class users::admins{
+  users::managed_user {'joe':}
+  users::managed_user {'alice':
+    group => 'staff',
+    }
+    group { 'staff':
+      ensure => present,
+    
+    }
+
+
+}
